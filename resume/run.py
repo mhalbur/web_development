47c32168f90b58a4bd32585bf11e153bccfe3e5d
mhalbur <mhalbur.2013@gmail.com>
2020-05-16 21:52:42 -0500

    Checkpoint - 5/16/2020

--- resume/run.py
+++ resume/run.py
@@ -11,4 +11,4 @@ def about_me():
     return render_template("about_me.html")
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=5000)
